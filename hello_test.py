823b2a540e297b25c08e6ece13e78d90a5841169
plopezgit <p.lopez.ch.hr@hotmail.com>
2026-01-13 17:33:31 +0100

    hello test

---
+++ hello_test.py
@@ -0,0 +1,30 @@
+print("Test")
+
+message = "Soy un test"
+
+print(message)
+
+feedback = "De acuerdo. Que tipo de Test?"
+
+print(feedback)
+
+message = "Smoke"
+
+print(message)
+
+feedback = "Como te llamas?"
+
+print(feedback)
+
+message = ("Smoke Test")
+
+print (message.title())
+
+print (message.upper())
+print (message.lower())
+
+thank = "merci, "
+
+final = f"{thank}{message}"
+
+print(f"\tMuy bien, {final.title()}, \n\tnext!")
